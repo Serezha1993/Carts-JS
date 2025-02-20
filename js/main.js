0f2d61023ac1570b9cf6261490bc03f92660e026
Serezha1993 <007serzh2014@mail.ru>
2025-02-20 12:06:07 +0300

    pagination

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+import products from "./products.js";
 
+import { renderProductCards } from "./modules/productCards.js";
+
+import { paginate } from "./modules/pagination.js";
+
+window.addEventListener('DOMContentLoaded', () => {
+    const productContainer = document.querySelector('.js-products-list');
+
+    // renderProductCards(products, productContainer);
+    paginate(products);
+})
